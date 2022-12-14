573b1741080ae8be46a652008700959f0df0963f
Be-student <presentsong@naver.com>
2022-12-14 21:33:36 +0900

    feat: 결과 dto를 추가한다

--- src/main/java/subway/adapter/in/client/InputView.java
+++ src/main/java/subway/adapter/in/client/InputView.java
@@ -11,6 +11,7 @@ public class InputView {
     }
 
     public GameCommand askRetry() {
+        System.out.println();
         System.out.println("## 메인 화면");
         System.out.println("1. 경로 조회");
         System.out.println("Q. 종료");
