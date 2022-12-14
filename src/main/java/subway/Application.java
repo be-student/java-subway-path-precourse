4f14f3adee8103f755eff9b1ecd95f3076c580f7
Be-student <presentsong@naver.com>
2022-12-14 21:19:58 +0900

    feat(Adapter): 입력 기능을 추가한다

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,15 @@
 package subway;
 
 import java.util.Scanner;
+import subway.adapter.in.client.InputView;
+import subway.adapter.in.client.SubwayClient;
+import subway.application.SubwayApplication;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        // TODO: 프로그램 구현
+        InputView inputView=new InputView(scanner);
+        SubwayClient subwayClient = new SubwayClient(new SubwayApplication(), inputView);
+        subwayClient.run();
     }
 }
---
+++ src/main/java/subway/adapter/in/client/SubwayClient.java
@@ -0,0 +1,39 @@
+package subway.adapter.in.client;
+
+import java.util.function.Supplier;
+import subway.application.port.in.SubwayUseCase;
+
+public class SubwayClient {
+    private final SubwayUseCase subwayUseCase;
+    private final InputView inputView;
+
+    public SubwayClient(SubwayUseCase subwayUseCase, InputView inputView) {
+        this.inputView=inputView;
+        this.subwayUseCase = subwayUseCase;
+    }
+
+    public void run(){
+        GameCommand playerInput=GameCommand.START;
+        while (playerInput != GameCommand.QUIT) {
+            calculateDistance();
+            playerInput= askRetry();
+        }
+    }
+
+    private void calculateDistance() {
+
+    }
+
+    private GameCommand askRetry() {
+        return repeat(inputView::askRetry);
+    }
+
+    private  <T> T repeat(Supplier<T> playerInput){
+        try {
+            return playerInput.get();
+        } catch (IllegalArgumentException e) {
+            System.out.print(e.getMessage());
+            return repeat(playerInput);
+        }
+    }
+}
